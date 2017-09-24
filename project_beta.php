5033dd3a736f3b3d8cf0abef35f064aef9e0797b
luca <lscarci17@gmail.com>
2017-09-24 00:15:46 -0400

    Adjusted Project Page
    Adjusted the height of the project overview section.
    Adjusted placehodler content in overview

--- project_beta.php
+++ project_beta.php
@@ -37,7 +37,7 @@
 					<li class="project-info__list-left"><span class="light-list">Other:</span></li>
 				</div>
 
-				<div class="project-info__center">Climate Change is happening, and people need to believe it. Visualizing it in the simplest form is an excellent method of delivering the message. Using three.js (a library extension of WebGL) and basic JavaScript elements, we we’re tasked with visualizing some climate change, into an informational and educational way. I focused on the leading causes of climate change, and the potential threats they pose, as well as the possible ways to counter it.</div>
+				<div class="project-info__center">Using three.js (a library extension of WebGL) and basic JavaScript elements, we we’re tasked with visualizing some climate change, into an informational and educational way. I focused on the leading causes of climate change, and the potential threats they pose, as well as the possible ways to counter it.</div>
 
 				<div class="project-info__right">
 					<li class="project-info__list-right">> Pantone Refit</li>
